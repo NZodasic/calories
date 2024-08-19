edba97f25e274930d8c7b1105262130c19ff85bf
Nguyễn Võ Chí Dũng <145178800+NZodasic@users.noreply.github.com>
2024-08-19 15:15:33 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { FoodProvider } from './context/FoodContext'
-import Header from './components/food/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Header/> */}
   <FoodProvider>
     <App />
     </FoodProvider>
