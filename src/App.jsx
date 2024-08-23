580f64664b2484a27497331bd29ba7b8a8e082ee
Nguyễn Võ Chí Dũng <145178800+NZodasic@users.noreply.github.com>
2024-08-23 09:04:36 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -157,7 +157,7 @@ function App() {
       <footer className="app-footer">
         <div className="footer-content">
         <ul className="footer-menu">
-        <h1 className="footer-item" onClick={() => window.location.href = "https://foodrecognltion.vercel.app"}>Home</h1>
+        <h1 className="footer-item" onClick={() => window.location.href = "https://foodrecognltion.vercel.app/home"}>Home</h1>
         <h1 className="footer-item" onClick={() => window.location.href = "http://localhost:3000/upload"}>Scanning food</h1>
         <h1 className="footer-item" onClick={() => window.location.href = "https://foodrecognition.vercel.app/recives"}>Receive</h1>
         </ul>
